dbab685be2f888fa1fd502ba2e21aaba92382d89
joelynch-1 <lynch.jos@northeastern.edu>
2020-03-31 19:24:45 -0400

    Update table names to allow django to read from them

--- InsertTestValues.sql
+++ InsertTestValues.sql
@@ -1,20 +1,20 @@
 USE spotify_copycatDB;
 
-INSERT INTO SpotifyUser (username, pass, firstName, dateJoined) VALUES
+INSERT INTO spotify_SpotifyUser (username, pass, firstName, dateJoined) VALUES
 ('User1', 'password123', 'Tester1', '2020-03-28'),
 ('User2', 'password123', 'Tester2', '2020-03-28');
 
-INSERT INTO Playlist (playlistId, playlistName, createdBy) VALUES
+INSERT INTO spotify_Playlist (playlistId, playlistName, createdBy) VALUES
 (NULL, 'CountryPlaylist', 'User1'),
 (NULL, 'RockPlaylist', 'User1'),
 (NULL, 'PartyPlaylist', 'User2');
 
-INSERT INTO Genre (genreID, genre) VALUES
+INSERT INTO spotify_Genre (genreID, genre) VALUES
 (NULL, 'Country'),
 (NULL, 'Rock'),
 (NULL, 'Rap');
 
-INSERT INTO Artist (artistName, mainGenre) VALUES
+INSERT INTO spotify_Artist (artistName, mainGenre) VALUES
 ('High Valley', 1),
 ('Florida Georgia Line', 1),
 ('Van halen', 2),
@@ -22,7 +22,7 @@ INSERT INTO Artist (artistName, mainGenre) VALUES
 ('G-Eazy', 3),
 ('Eminem', 3);
 
-INSERT INTO Album (albumID, title, artist, dateReleased) VALUES
+INSERT INTO spotify_Album (albumID, title, artist, dateReleased) VALUES
 (NULL, 'Dig Your Roots', 'Florida Georgia Line', '2016-01-01'),
 (NULL, 'County Line', 'High Valley', '2016-01-01'),
 (NULL, '5150', 'Van Halen', '1986-01-01'),
@@ -30,7 +30,7 @@ INSERT INTO Album (albumID, title, artist, dateReleased) VALUES
 (NULL, 'These Things Happen', 'G-Eazy', '2014-01-01'),
 (NULL, 'Curtain Call', 'Eminem', '2005-01-01');
 
-INSERT INTO Song (songID, title, duration, genreID, artist, albumID) VALUES
+INSERT INTO spotify_Song (songID, title, duration, genreID, artist, albumID) VALUES
 (NULL, 'Smooth', 170, 1, 'Florida Georgia Line', 1),
 (NULL, 'Dig Your Roots', 213, 1, 'Florida Georgia Line', 1),
 (NULL, 'Farm Girl', 181, 1, 'High Valley', 2),
@@ -44,7 +44,7 @@ INSERT INTO Song (songID, title, duration, genreID, artist, albumID) VALUES
 (NULL, 'FACK', 206, 3, 'Eminem', 6),
 (NULL, 'The Way I Am', 291, 3, 'Eminem', 6);
 
-INSERT INTO PlaylistSongs (songID, playlistID) VALUES
+INSERT INTO spotify_PlaylistSongs (songID, playlistID) VALUES
 (1, 1),
 (2, 1),
 (3, 1),
@@ -56,12 +56,12 @@ INSERT INTO PlaylistSongs (songID, playlistID) VALUES
 (9, 3),
 (10, 3);
 
-INSERT INTO UserFollow (username, artist) VALUES
+INSERT INTO spotify_UserFollow (username, artist) VALUES
 ('User1', 'High Valley'),
 ('User1', 'Van Halen'),
 ('User1', 'G-Eazy');
 
-INSERT INTO UserLikes (username, songID) VALUES
+INSERT INTO spotify_UserLikes (username, songID) VALUES
 ('User1', 1),
 ('User1', 2),
 ('User1', 4),
