a55f8db5b00977e0dca9b5ed3572202c67d6b606
joelynch-1 <lynch.jos@northeastern.edu>
2020-04-05 11:04:35 -0400

    Fixed value sql script

--- InsertTestValues.sql
+++ InsertTestValues.sql
@@ -5,14 +5,14 @@ INSERT INTO spotify_SpotifyUser (username, pass, firstName, dateJoined) VALUES
 ('User2', 'password123', 'Tester2', '2020-03-28');
 
 INSERT INTO spotify_Playlist (playlistId, playlistName, createdBy) VALUES
-(NULL, 'CountryPlaylist', 'User1'),
-(NULL, 'RockPlaylist', 'User1'),
-(NULL, 'PartyPlaylist', 'User2');
+(1, 'CountryPlaylist', 'User1'),
+(2, 'RockPlaylist', 'User1'),
+(3, 'PartyPlaylist', 'User2');
 
 INSERT INTO spotify_Genre (genreID, genre) VALUES
-(NULL, 'Country'),
-(NULL, 'Rock'),
-(NULL, 'Rap');
+(1, 'Country'),
+(2, 'Rock'),
+(3, 'Rap');
 
 INSERT INTO spotify_Artist (artistName, mainGenre) VALUES
 ('High Valley', 1),
@@ -23,26 +23,26 @@ INSERT INTO spotify_Artist (artistName, mainGenre) VALUES
 ('Eminem', 3);
 
 INSERT INTO spotify_Album (albumID, title, artist, dateReleased) VALUES
-(NULL, 'Dig Your Roots', 'Florida Georgia Line', '2016-01-01'),
-(NULL, 'County Line', 'High Valley', '2016-01-01'),
-(NULL, '5150', 'Van Halen', '1986-01-01'),
-(NULL, 'Appetite For Destruction', 'Guns N\' Roses', '1987-01-01'),
-(NULL, 'These Things Happen', 'G-Eazy', '2014-01-01'),
-(NULL, 'Curtain Call', 'Eminem', '2005-01-01');
+(1, 'Dig Your Roots', 'Florida Georgia Line', '2016-01-01'),
+(2, 'County Line', 'High Valley', '2016-01-01'),
+(3, '5150', 'Van Halen', '1986-01-01'),
+(4, 'Appetite For Destruction', 'Guns N\' Roses', '1987-01-01'),
+(5, 'These Things Happen', 'G-Eazy', '2014-01-01'),
+(6, 'Curtain Call', 'Eminem', '2005-01-01');
 
 INSERT INTO spotify_Song (songID, title, duration, genreID, artist, albumID) VALUES
-(NULL, 'Smooth', 170, 1, 'Florida Georgia Line', 1),
-(NULL, 'Dig Your Roots', 213, 1, 'Florida Georgia Line', 1),
-(NULL, 'Farm Girl', 181, 1, 'High Valley', 2),
-(NULL, 'Be You', 188, 1, 'High Valley', 2),
-(NULL, 'Good Enough', 243, 2, 'Van Halen', 3),
-(NULL, 'Get Up', 283, 2, 'Van Halen', 3),
-(NULL, 'Welcome to the Jungle', 273, 2, 'Guns N\' Roses', 4),
-(NULL, 'Sweet Child O\' Mine', 356, 2, 'Guns N\' Roses', 4),
-(NULL, 'These Things Happen', 144, 3, 'G-Eazy', 5),
-(NULL, 'Almost Famous', 269, 3, 'G-Eazy', 5),
-(NULL, 'FACK', 206, 3, 'Eminem', 6),
-(NULL, 'The Way I Am', 291, 3, 'Eminem', 6);
+(1, 'Smooth', 170, 1, 'Florida Georgia Line', 1),
+(2, 'Dig Your Roots', 213, 1, 'Florida Georgia Line', 1),
+(3, 'Farm Girl', 181, 1, 'High Valley', 2),
+(4, 'Be You', 188, 1, 'High Valley', 2),
+(5, 'Good Enough', 243, 2, 'Van Halen', 3),
+(6, 'Get Up', 283, 2, 'Van Halen', 3),
+(7, 'Welcome to the Jungle', 273, 2, 'Guns N\' Roses', 4),
+(8, 'Sweet Child O\' Mine', 356, 2, 'Guns N\' Roses', 4),
+(9, 'These Things Happen', 144, 3, 'G-Eazy', 5),
+(10, 'Almost Famous', 269, 3, 'G-Eazy', 5),
+(11, 'FACK', 206, 3, 'Eminem', 6),
+(12, 'The Way I Am', 291, 3, 'Eminem', 6);
 
 INSERT INTO spotify_PlaylistSongs (songID, playlistID) VALUES
 (1, 1),
