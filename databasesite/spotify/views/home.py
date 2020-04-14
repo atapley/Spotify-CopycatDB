7f34eb3b24e60c2d4243b7bb1867b9a09607af64
joelynch-1 <lynch.jos@northeastern.edu>
2020-04-14 12:24:37 -0400

    Check for user before getting first name

--- databasesite/spotify/views/home.py
+++ databasesite/spotify/views/home.py
@@ -15,10 +15,10 @@ def startscreen(request, name):
 def login_test(request):
     username = request.POST['username']
     password = request.POST['password']
-    first_name = SpotifyUser.objects.filter(Q(username=username)).values()[0]['firstName']
 
     user_query = SpotifyUser.objects.filter(Q(username=username) & Q(password=password)).values()
     if user_query:
+        first_name = SpotifyUser.objects.filter(Q(username=username)).values()[0]['firstName']
         user = UserModel._default_manager.get_by_natural_key(username)
         login(request, user)
         return HttpResponseRedirect('/user_home/' + first_name)
