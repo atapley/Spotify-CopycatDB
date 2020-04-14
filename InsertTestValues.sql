f5fe2738369d189cf226103d76c5602538b0bfa6
atapley <tapleyalx@gmail.com>
2020-04-13 20:09:18 -0400

    Removed table 'album' and created constraints to not allow empty inputs

--- InsertTestValues.sql
+++ InsertTestValues.sql
@@ -22,27 +22,19 @@ INSERT INTO spotify_Artist (artistName, mainGenre) VALUES
 ('G-Eazy', 3),
 ('Eminem', 3);
 
-INSERT INTO spotify_Album (albumID, title, artist, dateReleased) VALUES
-(1, 'Dig Your Roots', 'Florida Georgia Line', '2016-01-01'),
-(2, 'County Line', 'High Valley', '2016-01-01'),
-(3, '5150', 'Van Halen', '1986-01-01'),
-(4, 'Appetite For Destruction', 'Guns N\' Roses', '1987-01-01'),
-(5, 'These Things Happen', 'G-Eazy', '2014-01-01'),
-(6, 'Curtain Call', 'Eminem', '2005-01-01');
-
-INSERT INTO spotify_Song (songID, title, duration, genreID, artist, albumID) VALUES
-(1, 'Smooth', 170, 1, 'Florida Georgia Line', 1),
-(2, 'Dig Your Roots', 213, 1, 'Florida Georgia Line', 1),
-(3, 'Farm Girl', 181, 1, 'High Valley', 2),
-(4, 'Be You', 188, 1, 'High Valley', 2),
-(5, 'Good Enough', 243, 2, 'Van Halen', 3),
-(6, 'Get Up', 283, 2, 'Van Halen', 3),
-(7, 'Welcome to the Jungle', 273, 2, 'Guns N\' Roses', 4),
-(8, 'Sweet Child O\' Mine', 356, 2, 'Guns N\' Roses', 4),
-(9, 'These Things Happen', 144, 3, 'G-Eazy', 5),
-(10, 'Almost Famous', 269, 3, 'G-Eazy', 5),
-(11, 'FACK', 206, 3, 'Eminem', 6),
-(12, 'The Way I Am', 291, 3, 'Eminem', 6);
+INSERT INTO spotify_Song (songID, title, duration, genreID, artist) VALUES
+(1, 'Smooth', 170, 1, 'Florida Georgia Line'),
+(2, 'Dig Your Roots', 213, 1, 'Florida Georgia Line'),
+(3, 'Farm Girl', 181, 1, 'High Valley'),
+(4, 'Be You', 188, 1, 'High Valley'),
+(5, 'Good Enough', 243, 2, 'Van Halen'),
+(6, 'Get Up', 283, 2, 'Van Halen'),
+(7, 'Welcome to the Jungle', 273, 2, 'Guns N\' Roses'),
+(8, 'Sweet Child O\' Mine', 356, 2, 'Guns N\' Roses'),
+(9, 'These Things Happen', 144, 3, 'G-Eazy'),
+(10, 'Almost Famous', 269, 3, 'G-Eazy'),
+(11, 'FACK', 206, 3, 'Eminem'),
+(12, 'The Way I Am', 291, 3, 'Eminem');
 
 INSERT INTO spotify_PlaylistSongs (songID, playlistID) VALUES
 (1, 1),
